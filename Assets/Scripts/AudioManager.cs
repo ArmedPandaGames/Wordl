0f9504900c1da898758c64ce894a4c000ab798b4
ArmedPandaGames <armedpandagames@gmail.com>
2026-03-02 23:44:01 +0200

    added music and mute option

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void MuteAudio()
+    {
+        if (audioSource != null)
+        {
+            audioSource.mute = !audioSource.mute;
+        }
+    }
+}
